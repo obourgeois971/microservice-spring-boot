f50c48740dba3c8c9e051215a11eca9dc4cea55f
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-10-01 18:40:31 +0200

    initialization of gateway

---
+++ api-gateway/src/main/java/com/programming/techie/gateway/routes/Routes.java
@@ -0,0 +1,46 @@
+package com.programming.techie.gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+	/*@Value("${product.service.url}")
+    private String productServiceUrl;
+    @Value("${order.service.url}")
+    private String orderServiceUrl;
+    @Value("${inventory.service.url}")
+    private String inventoryServiceUrl;*/
+    
+	@Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
+        /*return GatewayRouterFunctions.route("product_service")
+                .route(RequestPredicates.path("/api/product"), HandlerFunctions.http(productServiceUrl))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();*/
+		return GatewayRouterFunctions.route("product_service")
+                .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
+                .build();
+    }
+	
+	@Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+		return GatewayRouterFunctions.route("order_service")
+                .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
+                .build();
+    }
+	
+	@Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+		return GatewayRouterFunctions.route("inventory_service")
+                .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+}
